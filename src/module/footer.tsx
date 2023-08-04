faf890302ee9530820921495da8ebaa7be4f3e25
HRS8 <harshasai3789@gmail.com>
2023-08-05 01:54:41 +0530

    ini

---
+++ src/module/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/skeleton.css';
+import './css/skeleton.css';
+
+function Footer(): JSX.Element {
+  return (
+    <div className="Footer container">
+      <p>
+        Made wit <span className="heart">❤️</span> by <a href={'https://twitter.com/heysagnik'}>Harsha</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
